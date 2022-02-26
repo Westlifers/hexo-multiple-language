523c259f1baa59598f235ab565c06cbfc4f36fa5
Westlifers <2585687459@qq.com>
2022-02-26 16:58:02 +0800

    添加翻译等待，但不知等待多长时间，且出现莫名其妙的渲染错误

--- index.js
+++ index.js
@@ -27,29 +27,57 @@ hexo.extend.tag.register('mullan', (_args) => {
   // 合并这些html代码，注意在语言切换框前截断摘要，防止出现在首页
   let Html = '<div id="shiftpara">' + shiftparaHtml + '<!--more-->' + boxHtml + '</div>' + vueHtml
 
-  // 注意返回前清空本地变量，防止变量跨文章！
+  // 注意返回前清空相关本地变量，防止变量跨文章！
   hexo.locals.set('languages', [])
+  hexo.locals.set('source', '')
   return Html
 
 }, {ends: false})
 
+
+
 // language标签，覆盖一段文本，读取语言项
-hexo.extend.tag.register('language', (args, content) => {
+hexo.extend.tag.register('language', async (args, content) => {
   // 从本地变量读取languages
   let languages = hexo.locals.get('languages')
   // 从文章本标签处读取本语言文本
   let language = args[0]
   let text = content
+  // 检查是否是源语言
+  if (args[1] == 'source'){
+    //若是，设置为本地变量
+    hexo.locals.set('source', language)
+  }
+  // 检查是否需要翻译
+  else if(args[1] == 'translate'){
+    // 若是，获取源语言并翻译文本
+    var source = hexo.locals.get('source')
+    for (let lan of hexo.locals.get('languages')){
+      if (lan.language == source){
+        var source_text = lan.text
+      }
+    }
+    // 开始翻译，但是要考虑等待时间
+    // 因为百度翻译的请求限制，一秒只能请求一次，实际测试发现间隔一秒仍然有可能超频
+    // 需要设置一个链条，每个translate函数都要在这个链条上sleep，一个比一个长
+    // 从本地变量获取本次休眠时间
+    var sleepTime = hexo.locals.get('sleepTime')
+    if (!sleepTime && typeof(sleepTime) != 'number'){sleepTime = 0}         // js变量类型太日怪了我去
+    else{sleepTime += 2000}                                                 // 这个等待时长需要好好琢磨一下
+    // 获取到后更新睡觉时间
+    hexo.locals.set('sleepTime', sleepTime)
+    text = await translate(source_text, 'auto', language, sleepTime)       // 注意！这里还不太对，language并不是百度翻译能识别的语言，需要用表转换！注意改！或者说在readme中给出这张表！
+  }
   let one_mullan = {language: language, text: text}
   // 将本语言加入到本地变量languages中
   if (!languages){languages = [one_mullan]}
   else{languages.push(one_mullan)}
   hexo.locals.set('languages', languages)
-}, {ends: true})
+}, {ends: true, async: true})
 
 
 
-// 在生成器中注册box_asset事件，将box.css与box.js读取到source文件夹当中
+// 在生成器中注册box_asset事件，将box.css取到source文件夹当中
 hexo.extend.generator.register('box_asset', () => [
   {
     path: 'css/box.css',
